61433364733548e28904d981f7809dcbde2cd8aa
hunziker <thomas.hunziker@customweb.ch>
2013-02-27 09:59:51 +0100

    Add compatibility for java 1.6.

--- src/main/java/com/customweb/flyway/HibernateDataSource.java
+++ src/main/java/com/customweb/flyway/HibernateDataSource.java
@@ -3,6 +3,8 @@ package com.customweb.flyway;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
@@ -56,6 +58,12 @@ public class HibernateDataSource implements DataSource{
 	@Override
 	public Connection getConnection(String username, String password) throws SQLException {
 		return this.sessionFactory.getJdbcServices().getConnectionProvider().getConnection();
+	}
+
+
+	@Override
+	public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+		throw new SQLFeatureNotSupportedException();
 	}
 
 }
